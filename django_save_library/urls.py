e0e4ab006a1537e23e818e1e6c6105e4805d2c6d
chino0210 <79809046+chino0210@users.noreply.github.com>
2024-05-28 22:34:05 -0500

    Update urls.py

--- django_save_library/urls.py
+++ django_save_library/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('entrys_save.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
