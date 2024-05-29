83c7e84c9c7c0c26e079b51bb65a4a4aca6ebc18
chino0210 <79809046+chino0210@users.noreply.github.com>
2024-05-28 22:49:15 -0500

    Update settings.py

--- django_save_library/settings.py
+++ django_save_library/settings.py
@@ -127,11 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
